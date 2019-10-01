f15f1b7415a23c7d6c87c51d0f0646bcf20befdc
Eléonore Dupoix <eleonore.dupoix@gmail.com>
2019-10-01 17:49:29 +0200

    Add db_pool, migration, migration script

---
+++ db_pool.js
@@ -0,0 +1,9 @@
+const pg = require("pg");
+require("dotenv").config();
+const databaseUrl = process.env.DATABASE_URL;
+
+const pool = new pg.Pool({
+  connectionString: databaseUrl
+});
+
+module.exports = pool;
--- index.js
+++ index.js
@@ -1,10 +1,11 @@
-const express = require('express')
-const app = express()
+const express = require("express");
+const app = express();
+require("dotenv").config();
 
-app.get('/', function (req, res) {
-  res.send('Hello W!')
-})
+app.get("/", function(req, res) {
+  res.send("Hello W!");
+});
 
-app.listen(8000, function () {
-  console.log('Example app listening on port 8000!')
-})
+app.listen(8000, function() {
+  console.log("Example app listening on port 8000!");
+});
