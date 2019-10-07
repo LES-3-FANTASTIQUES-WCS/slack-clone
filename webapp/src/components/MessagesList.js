5efe610d13791b20d8a6e658b9f57e4bff090bdb
Helder Nascimento <nortehelder@gmail.Com>
2019-10-07 17:08:52 +0200

    Converted from function to React Component

--- webapp/src/components/MessagesList.js
+++ webapp/src/components/MessagesList.js
@@ -3,24 +3,71 @@ import { Comment } from 'semantic-ui-react';
 
 import { MessageWrapper } from './styles/MessagesList';
 
-export default ({ messages }) => (
-  <MessageWrapper>
-    <Comment.Group>
-      {messages.map(message => (
-        <Comment key={`${message.id}-message`}>
-          <Comment.Avatar as="a" src="https://picsum.photos/200" />
-          <Comment.Content>
-            <Comment.Author as="a">{message.username}</Comment.Author>
-            <Comment.Metadata>
-              <div>Today at 5:42PM</div>
-            </Comment.Metadata>
-            <Comment.Text>{message.text}</Comment.Text>
-            <Comment.Actions>
-              <Comment.Action>Reply</Comment.Action>
-            </Comment.Actions>
-          </Comment.Content>
-        </Comment>
-      ))}
-    </Comment.Group>
-  </MessageWrapper>
-);
+class MessagesList extends React.Component {
+  state = {
+    channelId: this.props.channelId,
+    isLoading: true,
+    messages: [],
+    shouldRefetchMessages: false,
+  };
+
+  fetchMessages = async () => {
+    this.setState({
+      shouldRefetchMessages: false,
+    });
+    const response = await fetch(
+      `/api/channels/${this.props.channelId}/messages`
+    );
+    const { messages } = await response.json();
+    this.setState({
+      isLoading: false,
+      messages,
+    });
+  };
+
+  componentDidMount() {
+    this.fetchMessages();
+  }
+
+  static getDerivedStateFromProps(nextProps, prevState) {
+    // will need to refetch whenever React Router passes another channel id without unmounting component
+    if (nextProps.channelId !== prevState.channelId) {
+      // equivalent to this.setState(…)
+      return { channelId: nextProps.channelId, shouldRefetchMessages: true };
+    }
+    return null;
+  }
+
+  componentDidUpdate() {
+    if (this.state.shouldRefetchMessages) {
+      this.fetchMessages();
+    }
+  }
+
+  render() {
+    if (this.state.isLoading) {
+      return <div>Loading…</div>;
+    }
+    return (
+      <MessageWrapper>
+        <Comment.Group>
+          {messages.map(message => (
+            <Comment key={`${message.id}-message`}>
+              <Comment.Avatar as="a" src="https://picsum.photos/200" />
+              <Comment.Content>
+                <Comment.Author as="a">{message.username}</Comment.Author>
+                <Comment.Metadata>
+                  <div>Today at 5:42PM</div>
+                </Comment.Metadata>
+                <Comment.Text>{message.text}</Comment.Text>
+                <Comment.Actions>
+                  <Comment.Action>Reply</Comment.Action>
+                </Comment.Actions>
+              </Comment.Content>
+            </Comment>
+          ))}
+        </Comment.Group>
+      </MessageWrapper>
+    );
+  }
+}
