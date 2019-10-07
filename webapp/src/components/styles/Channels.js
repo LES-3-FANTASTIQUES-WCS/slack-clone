0c9564806af9ba96400b84f3301267b2f367259c
Helder Nascimento <nortehelder@gmail.Com>
2019-10-07 13:46:09 +0200

    Add temporary style files for Channels, Header, Teams

---
+++ webapp/src/components/styles/Channels.js
@@ -0,0 +1,10 @@
+import styled from 'styled-components';
+
+const ChannelWrapper = styled.div`
+  grid-column: 2;
+  grid-row: 1 / 4;
+  background-color: #4e3a4c;
+  color: #958993;
+`;
+
+export { ChannelWrapper };
---
+++ webapp/src/components/styles/Header.js
@@ -0,0 +1,8 @@
+import styled from 'styled-components';
+
+const HeaderWrapper = styled.div`
+  grid-column: 3;
+  grid-row: 1;
+`;
+
+export { HeaderWrapper };
