d73d50988926b2f34847661d3d79a467fff355ae
Helder Nascimento <nortehelder@gmail.Com>
2019-10-08 12:19:41 +0200

    Add SendMessage Methods and state

--- webapp/src/components/SendMessage.js
+++ webapp/src/components/SendMessage.js
@@ -4,10 +4,41 @@ import { Input } from 'semantic-ui-react';
 import { SendMessageWrapper } from './styles/SendMessage';
 
 class SendMessage extends React.Component {
+  state = {
+    channelId: this.props.channelId,
+    text: '',
+    userId: 1,
+  };
+
+  onSubmit = async event => {
+    event.preventDefault();
+    const response = this.state;
+
+    console.log(response);
+  };
+
+  onChange = e => {
+    const { name, value } = e.target;
+    // name = "email";
+    this.setState({ [name]: value });
+  };
+
   render() {
+    const { text } = this.state;
+    const { onChange, onSubmit } = this;
+
     return (
       <SendMessageWrapper>
-        <Input fluid placeholder="Send message" />
+        <form onSubmit={onSubmit}>
+          <Input
+            name="text"
+            onChange={onChange}
+            value={text}
+            placeholder="Send message"
+            type="text"
+            fluid
+          />
+        </form>
       </SendMessageWrapper>
     );
   }
