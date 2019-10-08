234ae64c9a04b21a290c8e5b73691dab34153d04
lcvoisenet <lcvoisenet@gmail.com>
2019-10-08 10:41:10 +0200

    add assert

--- webapp/src/App.js
+++ webapp/src/App.js
@@ -1,31 +1,15 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
 import TheHeader from './components/Header/TheHeader';
 
-
-function App() {
-  return (
-    
-    <div className="App">
-      <TheHeader/>
-      
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+  render() {
+    return <TheHeader />;
+  }
 }
 
 export default App;
