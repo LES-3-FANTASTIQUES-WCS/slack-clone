d6b405fabd6190a7f5afef4b70e70ee6547ded9d
lcvoisenet <lcvoisenet@gmail.com>
2019-10-08 10:41:52 +0200

    add toggleBtn

---
+++ webapp/src/components/Header/ToggleBtn.js
@@ -0,0 +1,38 @@
+import React, { Component } from 'react';
+import Logo from '../../assert/logo.svg';
+import { Responsive, Button, Image, Header } from 'semantic-ui-react';
+
+class ToggleBtn extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { isToggleOn: true };
+  }
+
+  displaySide = () => {
+    this.setState(state => ({
+      isToggleOn: !state.isToggleOn,
+    }));
+  };
+  render() {
+    return (
+      <div style={{ display: 'flex', flex: 'left' }}>
+        <Responsive as={Header} maxWidth={992}>
+          <Button onClick={this.displaySide} />
+          {this.state.isToggleOn ? 'ON' : 'OFF'}
+        </Responsive>
+        <Responsive as={Header} minWidth={992}>
+          <a
+            onClick=""
+            href="https://fr.reactjs.org/"
+            target="_blank"
+            rel="noreferrer"
+          >
+            <Image size="mini" src={Logo} />
+          </a>
+        </Responsive>
+      </div>
+    );
+  }
+}
+
+export default ToggleBtn;
