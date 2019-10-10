e2bbd36ecd3bccba23b47277ec14a3a849074102
Helder Nascimento <nortehelder@gmail.Com>
2019-10-10 16:15:32 +0200

    Build webapp server with Node.js server

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 
 const app = express();
+const path = require('path');
 
 const bodyParser = require('body-parser');
 const routes = require('./routes');
@@ -13,6 +14,10 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 app.use('/api', routes);
+app.use(express.static(path.join(__dirname, 'webapp', 'build')));
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'webapp', 'build', 'index.html'));
+});
 
 app.listen(port, () => {
   console.log(`App running on port ${port}.`);
