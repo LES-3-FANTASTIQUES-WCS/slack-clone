aabce52e330eac3519a22ffcd586c0996e3dc5be
Helder Nascimento <helder@lifeaz.fr>
2019-12-10 15:56:25 +0100

    Update - Updated Router

--- webapp/src/App.js
+++ webapp/src/App.js
@@ -1,12 +1,39 @@
 import React from 'react';
+import { BrowserRouter as Router, Link, Route } from 'react-router-dom';
 
+import PrivateRoute from './PrivateRoute';
+import { AuthContext } from './context/auth';
+
+import Home from './components/Home';
+import Admin from './components/Admin';
 import Dashboard from './components/Dashboard';
+import Login from './pages/Login';
+import Signup from './pages/Signup';
 
-function App() {
+function App(props) {
   return (
-    <div className="App">
-      <Dashboard />
-    </div>
+    <AuthContext.Provider value={false}>
+      <Router>
+        <div>
+          <ul>
+            <li>
+              <Link to="/">Home Page</Link>
+            </li>
+            <li>
+              <Link to="/admin">Admin Page</Link>
+            </li>
+            <li>
+              <Link to="/dashboard">Dashboard Page</Link>
+            </li>
+          </ul>
+          <Route exact path="/" component={Home} />
+          <Route path="/login" component={Login} />
+          <Route path="/signup" component={Signup} />
+          <PrivateRoute path="/admin" component={Admin} />
+          <PrivateRoute path="/dashboard" component={Dashboard} />
+        </div>
+      </Router>
+    </AuthContext.Provider>
   );
 }
 
