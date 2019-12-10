eb3ec1ab991a8531c598f23c6f7c7c6726467d87
Helder Nascimento <helder@lifeaz.fr>
2019-12-10 16:49:50 +0100

    Feature - Add Redirect To Referer After Login If Available

--- webapp/src/PrivateRoute.js
+++ webapp/src/PrivateRoute.js
@@ -3,13 +3,19 @@ import { Route, Redirect } from 'react-router-dom';
 import { useAuth } from './context/auth';
 
 function PrivateRoute({ component: Component, ...rest }) {
-  const isAuthenticated = useAuth();
+  const { authTokens } = useAuth();
 
   return (
     <Route
       {...rest}
       render={props =>
-        isAuthenticated ? <Component {...props} /> : <Redirect to="/login" />
+        authTokens ? (
+          <Component {...props} />
+        ) : (
+          <Redirect
+            to={{ pathname: '/login', state: { referer: props.location } }}
+          />
+        )
       }
     />
   );
